9fd98a2f5a5cec950c06206206578ba3125b0ec7
Anzwur <matt.anzwur@gmail.com>
2015-01-17 22:52:13 -0600

    Updates

--- BinaryWatch/main.cpp
+++ BinaryWatch/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
+void ConvertToBinary(int n)
+{
+    if (n / 2 != 0) {
+        ConvertToBinary(n / 2);
+    }
+    printf("%d", n % 2);
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    ConvertToBinary(2);
     return 0;
 }
